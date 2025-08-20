b73dbedb7b132311936665c61ee752d53608f98c
Kamoliddin Hamidullayev <kamoliddinxamidullayev91@gmail.com>
2025-08-21 03:22:35 +0500

    Initial commit with model

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# title
+st.title('Mashina, Samalyot va Kemani klassifikatsiya qiluvchi model')
+
+# rasm yuklash
+file = st.file_uploader('Rasm yuklang:', type=['png', 'jpeg'])
+
+if file:
+    st.image(file)
+    
+    #PIL conver
+    img = PILImage.create(file)
+
+    # modelni chaqirish
+    model = load_learner('modelll.pkl')
+
+    # bashorat qilish
+    pred, pred_id, probs = model.predict(img)
+
+    # natijani chiqarish
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
+
